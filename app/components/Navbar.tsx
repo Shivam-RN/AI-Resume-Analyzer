ba53a032e1bbfcdb5a5a16b01588e27079032e6a
Shivam-RN <shivamrana1809@gmail.com>
2025-07-26 14:52:20 +0530

    fetching reume on home page and wide data functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,21 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navbar flex flex-col sm:flex-row items-center justify-between gap-4 px-4 py-3">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">ResumeMentor</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+            <div className="flex gap-4">
+                <Link to="/upload" className="primary-button w-fit">
+                    Upload Resume
+                </Link>
+                <Link to="/auth" className="primary-button w-fit">
+                    Logout
+                </Link>
+            </div>
         </nav>
-    )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
